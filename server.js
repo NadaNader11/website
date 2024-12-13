2b23843274ddd0d85f04d8624171753e8422a2c6
NadaNader11 <NG2201489@tkh.edu.eg>
2024-12-13 04:18:51 +0200

    webb

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const db = new sqlite.Database("./booking.db",sqlite.OPEN_READWRITE, (err) => {
     console.log('Connected to the database.');
 }) ;
 const cors = require('cors');
-
-Server.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}));
-
 app.use(cors());
 app.use(bodyparser.json());
-//get request
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));  
+
 app.get('/api/Bookings', (req, res) => {
     var sql = "SELECT * FROM GetBookings";
     console.log(req.url);
@@ -31,13 +30,78 @@ app.get('/api/Bookings', (req, res) => {
         res.json({ data: rows });
     });
 });
+app.get('/api/GetSpecialties', (req, res) => {
+    var sql = "SELECT * FROM Specialty order by SpecialtyName";
+    console.log(req.url);
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: rows });
+    });
+});
+
+
+app.post('/api/AddDoctor', (req, res) => {
+    const data = req.body;
+    console.log("Body=" + req.body);
+    console.log(data);
+    if (data.DoctorName == undefined || data.email == undefined || data.SpecialtyID == undefined || data.DegreeID == undefined || data.DegreeID == undefined) {
+        res.status(400).send('Required field is missing');
+    } else {
+
+    }
+    var sql = "INSERT INTO Doctors (DoctorName,email,SpecialtyID,DegreeID,MobileNo) VALUES('" + data.DoctorName + "','" + data.email + "'," + data.SpecialtyID + "," + data.DegreeID + "," + data.MobileNo + ");";
+    console.log(sql);
+    db.run(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Doctor inserted successfully");
+        // Send a response
+        res.send('Doctor inserted successfully!');
+     });
+  });
+
+
+  app.post('/api/AddSpecialty', (req, res) => {
+    const data = req.body;
+    console.log("Body=" + req.body);
+    console.log(data);
+    if (data.SpecialtyName == undefined) {
+        res.status(400).send('Specialty Name is required');
+    } else {
+        var sql = "INSERT INTO Specialty (SpecialtyName) VALUES('" + data.SpecialtyName + "');";
+        console.log(req.url);
+        db.run(sql, function (err, result) {
+            if (err) throw err;
+            // Send a response
+            console.log("Specialty inserted successfully");
+            res.send('Specialty received successfully!');
+         });
+    }
+     
+    
+  });
+  app.post('/api/AddDegree', (req, res) => {
+    const data = req.body;
+    console.log("Body=" + req.body);
+    console.log(data);  
+    if (data.DegreeName == undefined) {
+        res.status(400).send('Degree Name is required');
+    } else {
+        
+        var sql = "INSERT INTO Degree (DegreeName) VALUES('" + data.DegreeName + "');";
+        
+        db.run(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Degree inserted successfully");
+            // Send a response
+            res.send('Degree inserted successfully!');
+        });      
+    }
+  });
 console.log('App created.');
 app.listen(port, (err) => {
     if (err) return console.error(err);
     console.log(`Server is running on port ${port}`);
 }) ;
-
-
-
-
 
